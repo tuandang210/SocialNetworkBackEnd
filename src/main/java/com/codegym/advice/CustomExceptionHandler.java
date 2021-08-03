377b772a0788969252887f83ae070d637162292d
thanhhungcf268 <thanhhungcf268@gmail.com>
2021-08-03 09:22:04 +0700

    ok

--- src/main/java/com/codegym/advice/CustomExceptionHandler.java
+++ src/main/java/com/codegym/advice/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package com.codegym.advice;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
